cb27fca4c73dc4c7d3f2b9c952c6327aaafe060c
Lê Minh Quân <marucube35@gmail.com>
2022-08-06 09:53:28 +0700

    feat: add static file and sass

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const morgan = require('morgan')
 const { engine } = require('express-handlebars')
 const path = require('path')
-
 const app = express()
 const port = 3000
+
+// Static files
+app.use(express.static(path.join(__dirname, 'public')))
 
 // HTTP logger
 app.use(morgan('tiny'))
@@ -14,6 +16,7 @@ app.engine('hbs', engine({ extname: 'hbs' }))
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'resources/views'))
 
+// Routes
 app.get('/', (req, res) => {
     res.render('home')
 })
